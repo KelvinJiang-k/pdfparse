020a471de8b89607d5aafe1ccf9f941288d30253
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-09-23 19:12:59 -0700

    dashboard file display

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { Dialog, DialogContent, DialogTrigger } from '@radix-ui/react-dialog'
+import { useState } from 'react'
+import { Button } from './ui/button'
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v)
+        }
+      }}
+    >
+      <DialogTrigger
+        onClick={() => {
+          setIsOpen(true)
+        }}
+        asChild
+      >
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default UploadButton
