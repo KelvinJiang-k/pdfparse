d7a6ba0b2ae16686c840e2eb31b2a64b8565a674
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-09-26 15:58:37 -0700

    upload button functionality

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -39,7 +39,7 @@ const Page = async ({ params }: PageProps) => {
           </div>
         </div>
 
-        {/* chat box */}
+        {/* chat box */} 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-1 lg:border-t-0 '>
           <ChatWrapper />
         </div>
