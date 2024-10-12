5565d68975197b74eb48b654e3992488938f1d76
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-10-12 00:58:38 -0700

    Implement Stripe subscription payments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function absoluteUrl(path: string) {
+  // client
+  if (typeof window !== 'undefined') return path
+  // server AND deployed on vercel
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
+  // server and localhost
+  return `http://localhost:${process.env.PORT ?? 3000}${path})`
 }
