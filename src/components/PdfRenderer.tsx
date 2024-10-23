4a745ca9a38ec725b946682bb912522bc26a805b
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-10-22 23:58:06 -0700

    Fix vercel deployment: alternative react-pdf worker

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -26,7 +26,10 @@ import {
 import Simplebar from 'simplebar-react'
 import PdfFullscreen from './PdfFullscreen'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 interface PdfRendererProps {
   url: string
